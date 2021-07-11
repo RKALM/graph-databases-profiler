9140d952d764c49a8ceab17a0f6b8033ac15f1e4
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2021-07-11 23:04:33 +0200

    Implementing the initial structure. Now I will have to complete the menu.

--- graphprofiler/src/Main.java
+++ graphprofiler/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    static int repetititons = 10; //the number of repetitions
+    static String crud = "create"; //create, remove or update
+    static String graphDatabse = "neo4j"; //neo4j, graphdb pr orientdb
+    public static Scanner in = new Scanner( System.in);
+
     public static void main(String args[]){
-        System.out.println("Hello Java");
+        System.out.println("Welcome to profiler");
+        System.out.println("===================");
+        menu();
+    }
+
+    public static void menu(){
+        System.out.println("How many repetitions you wish to do?");
+        repetititons = in.nextInt();
+        System.out.println("You decided to run " + repetititons + " repetitions");
+
+
     }
 }

4bad59fb5537fcc30a95bee2ea7c2568c6d669ec
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2021-07-13 11:41:09 +0200

    Reducting the redudancy
    it seems that a double repetition system was a too complicated structure and I made it a single repetition which is going to work just fine.

--- graphprofiler/src/Main.java
+++ graphprofiler/src/Main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    static int standarrepetitions = 10; //the statistical repetitions for accurancy. How many times the program will run.
-    static int repetititons = 100; //the number of repetitions of the operation
+    static int repetititons = 10; //the number of repetitions of the operation. for accuracy
     static String crud = "create"; //create, delete or update
     static String graphDatabse = "neo4j"; //neo4j, graphdb pr orientdb
     public static Scanner in = new Scanner( System.in);
@@ -15,7 +14,7 @@ public class Main {
     }
 
     public static void menu(){
-        System.out.println("How many repetitions you wish to do?");
+        System.out.println("How many repetitions, (for accuracy) you wish to do?");
         repetititons = Integer.parseInt(in.nextLine());
         System.out.println("You decided to run " + repetititons + " repetitions");
 
@@ -57,16 +56,16 @@ public class Main {
     } //end of menu
 
     static void run(){
-        for(int x = 0; x<standarrepetitions; x++){
+        for(int x = 0; x<repetititons; x++){
             if(graphDatabse.equalsIgnoreCase("graphdb")){
                 //run graphdb script
-                System.out.println("Running the GraphDB #" + x);
+                System.out.println("Running " + crud + " operation on the GraphDB #" + x);
             } else if(graphDatabse.equalsIgnoreCase("orientdb")){
                 //run orientdb script
-                System.out.println("Running the OrientDB #" + x);
+                System.out.println("Running " + crud + " operation on the OrientDB #" + x);
             } else {
                 //run neo4j script
-                System.out.println("Running the neo4j #" + x);
+                System.out.println("Running " + crud + " operation on the Neo4j #" + x);
             }
         }
     }

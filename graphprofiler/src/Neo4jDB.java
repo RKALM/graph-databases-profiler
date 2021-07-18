0f0aea9fdaedce6524c957ef7596a395dc048125
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2021-07-18 14:02:38 +0200

    We have handshake with Neo4j
    We just had a successful handshake with Neo4j. It is not a maven instance but a pure Java jar module.

---
+++ graphprofiler/src/Neo4jDB.java
@@ -0,0 +1,55 @@
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+
+import static org.neo4j.driver.Values.parameters;
+
+public class Neo4jDB implements AutoCloseable
+{
+    private final Driver driver;
+
+    public Neo4jDB( String uri, String user, String password )
+    {
+        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    //Very early version. Just to check if everything works.
+    public void createANode( final String message )
+    {
+        try ( Session session = driver.session() )
+        {
+            String greeting = session.writeTransaction( new TransactionWork<String>()
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    Result result = tx.run( "CREATE (a:Greeting) " +
+                                    "SET a.message = $message " +
+                                    "RETURN a.message + ', from node ' + id(a)",
+                            parameters( "message", message ) );
+                    return result.single().get( 0 ).asString();
+                }
+            } );
+            System.out.println( greeting );
+        }
+    }
+
+    //legacy code remains only as example.
+//    public static void main( String... args ) throws Exception
+//    {
+//        try ( HelloWorldExample greeter = new HelloWorldExample( "bolt://localhost:7687", "profiler", "profiler" ) )
+//        {
+//            greeter.printGreeting( "hello, world" );
+//        }
+//    }
+}
--- graphprofiler/src/Neo4jcrud.java
+++ graphprofiler/src/Neo4jcrud.java
@@ -3,7 +3,9 @@ public class Neo4jcrud {
     //CREATE operation on Neo4j. It returns to indicate that is has complete its cycle.
     static boolean create(){
         //do something
-        System.out.println("Creating a node on Neo4j");
+        Neo4jDB neo4j = new Neo4jDB( "bolt://localhost:7687", "profiler", "profiler" );
+        neo4j.createANode("Creating a node on Neo4j");
+        //System.out.println("Creating a node on Neo4j");
         return true;
     }
 

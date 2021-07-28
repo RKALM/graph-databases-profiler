0986105638197efdeccd9941a12c599bf8a1c437
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2021-07-28 10:26:43 +0200

    Profiling. Create operation on Neo4j 100K for 5 samples

--- mavenOrientDBProfiler/src/main/java/Dataholder.java
+++ mavenOrientDBProfiler/src/main/java/Dataholder.java
@@ -10,7 +10,7 @@ public class Dataholder {
 
 
     //Intervals
-    static boolean million_allowed = true; //if true the system uses 1 million intervals as well
+    static boolean million_allowed = false; //if true the system uses 1 million intervals as well
     static boolean N100k_allowed = true; //if true the system uses 100k intervals as well
     static boolean N10k_allowed = true; //if true the system uses 10k intervals as well
     static boolean N1000_allowed = true; //if true the system uses 1000 intervals as well
@@ -18,8 +18,8 @@ public class Dataholder {
 
 
     //Access to databases
-    static boolean databases_allowed = false; //when true allows connection to the databases
-    static boolean neo4j_script_test = false; //only for the CRUD part. the scripts will run with print statement
+    static boolean databases_allowed = true; //when true allows connection to the databases
+    static boolean neo4j_script_test = true; //only for the CRUD part. the scripts will run with print statement
     static boolean graphdb_script_test = false; //only for the CRUD part. the scripts will run with print statement
     static boolean orientdb_script_test = false; //only for the CRUD part. the scripts will run with print statement
 
@@ -29,7 +29,7 @@ public class Dataholder {
     static int countAddition = 0;
     static int countDeletion = 0;
     static int countUpdate = 0;
-    static String customSeed = "yolo1";
+    static String customSeed = "profiling";
     static String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
     static String dateSeed = date.replace("-", "");
 

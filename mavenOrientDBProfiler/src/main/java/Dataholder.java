33bd29c066f66c0cfdb3f35bdc64fbdad63f6498
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2021-07-28 13:24:23 +0200

    Profiling Delete operation on Neo4j
    1000 interval for 2 samples.

--- mavenOrientDBProfiler/src/main/java/Dataholder.java
+++ mavenOrientDBProfiler/src/main/java/Dataholder.java
@@ -11,8 +11,8 @@ public class Dataholder {
 
     //Intervals
     static boolean million_allowed = false; //if true the system uses 1 million intervals as well
-    static boolean N100k_allowed = true; //if true the system uses 100k intervals as well
-    static boolean N10k_allowed = true; //if true the system uses 10k intervals as well
+    static boolean N100k_allowed = false; //if true the system uses 100k intervals as well
+    static boolean N10k_allowed = false; //if true the system uses 10k intervals as well
     static boolean N1000_allowed = true; //if true the system uses 1000 intervals as well
     static boolean N100_allowed = true; //if true the system uses 100 intervals as well
 
@@ -29,7 +29,7 @@ public class Dataholder {
     static int countAddition = 0;
     static int countDeletion = 0;
     static int countUpdate = 0;
-    static String customSeed = "profiling";
+    static String customSeed = "profiling3";
     static String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
     static String dateSeed = date.replace("-", "");
 
